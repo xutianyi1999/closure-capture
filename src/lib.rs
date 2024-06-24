f47280ac9ec7c80dc28d4714686649a74ed4eb83
xutianyi <xutianyi1999@live.com>
2024-06-24 14:06:15 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,37 @@
 #[repr(transparent)]
-pub struct Wrap<T: ?Sized>(T);
+#[doc(hidden)]
+pub struct _Wrap<T: ?Sized>(pub T);
 
-macro_rules! wrap_process {
+#[macro_export]
+#[doc(hidden)]
+macro_rules! _wrap_process {
     ($arg:ident, $($args:tt)*) => {
-        wrap_process!($arg);
-        wrap_process!($($args)*);
+        _wrap_process!($arg);
+        _wrap_process!($($args)*);
     };
     (mut $arg:ident, $($args:tt)*) => {
-        wrap_process!($arg);
-        wrap_process!($($args)*);
+        _wrap_process!($arg);
+        _wrap_process!($($args)*);
     };
     (mut $arg:ident) => {
-        wrap_process!($arg);
+        _wrap_process!($arg);
     };
     ($arg:ident) => {
-        let $arg = crate::Wrap($arg);
+        let $arg = $crate::_Wrap($arg);
     };
     () => {};
 }
 
-macro_rules! move_process {
+#[macro_export]
+#[doc(hidden)]
+macro_rules! _move_process {
      ($arg:ident, $($args:tt)*) => {
-        move_process!($arg);
-        move_process!($($args)*);
+        _move_process!($arg);
+        _move_process!($($args)*);
     };
     (mut $arg:ident, $($args:tt)*) => {
-        move_process!(mut $arg);
-        move_process!($($args)*);
+        _move_process!(mut $arg);
+        _move_process!($($args)*);
     };
     (mut $arg:ident) => {
        let $arg = $arg;
@@ -39,36 +44,38 @@ macro_rules! move_process {
     () => {};
 }
 
+#[macro_export]
 macro_rules! closure {
     ([$($args1:tt)*] ($($args2:tt)*) $expr:expr) => {
         {
-            wrap_process!($($args1)*);
+            $crate::_wrap_process!($($args1)*);
 
             |$($args2)*| {
-                move_process!($($args1)*);
+                $crate::_move_process!($($args1)*);
                 $expr
             }
         }
     };
     ([$($args1:tt)*] ($($args2:tt)*) -> $ret:tt $expr:expr) => {
         {
-            wrap_process!($($args1)*);
+            $crate::_wrap_process!($($args1)*);
 
             |$($args2)*| -> $ret {
-                move_process!($($args1)*);
+                $crate::_move_process!($($args1)*);
                 $expr
             }
         }
     };
 }
 
+#[macro_export]
 macro_rules! async_block {
     ([$($args1:tt)*] async $block:block) => {
         {
-            wrap_process!($($args1)*);
+            $crate::_wrap_process!($($args1)*);
 
             async {
-                move_process!($($args1)*);
+                $crate::_move_process!($($args1)*);
                 $block
             }
         }
